39fe62cf60429a2d14d96d45f9f3310e8ddf9744
ImranSasudin <imransasudin@gmail.com>
2020-12-27 21:04:33 +0800

    Dashboard BE & FE

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use DB;
 
 class UserController extends Controller
 {
     public function dashboard()
     {
-        return view('dashboard');
+        if(Auth::user()->roles()->first()->name == 'super-admin'){
+            $modules = DB::table('modules')->get();
+
+            $permissions = DB::table('modules')
+                ->select('modules.id as module_id','permissions.name')
+                ->join('permissions','permissions.module_id','modules.id')
+                ->get();
+        } else {
+            $modules = DB::table('modules')
+            ->select('modules.id', 'modules.name')
+            ->join('permissions','modules.id','permissions.module_id')
+            ->join('role_has_permissions','role_has_permissions.permission_id','permissions.id')
+            ->join('model_has_roles','model_has_roles.role_id','role_has_permissions.role_id')
+            ->join('users','users.id','model_has_roles.model_id')
+            ->where('users.id', Auth::user()->id)
+            ->distinct()
+            ->get();
+
+            $permissions = DB::table('modules')
+            ->select('modules.id as module_id','permissions.name')
+            ->join('permissions','modules.id','permissions.module_id')
+            ->join('role_has_permissions','role_has_permissions.permission_id','permissions.id')
+            ->join('model_has_roles','model_has_roles.role_id','role_has_permissions.role_id')
+            ->join('users','users.id','model_has_roles.model_id')
+            ->where('users.id', Auth::user()->id)
+            ->get();
+        }
+
+        
+        
+        // dd($permissions);
+
+        return view('dashboard', [
+            'permissions' => $permissions,
+            'modules' => $modules,
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,27 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="pt-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white shadow-2xl sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    You're logged in as {{ Auth::user()->roles()->first()->name }}!
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="font-sans flex items-center justify-center bg-blue-darker w-full py-8">
+        <div class="overflow-hidden bg-white rounded max-w-xl w-full shadow-lg  leading-normal">
+        @foreach($modules as $module)
+            <div href="#" class="block group p-4 border-b">
+                <p class="font-bold text-lg mb-1 text-black">{{ $module->name }}</p>
+                @foreach($permissions as $permission)
+                    @if($permission->module_id == $module->id)
+                    <p class="bg-purple-500 inline-block px-2 rounded-lg cursor-default text-white mb-2 ml-2 transform hover:-translate-y-1 hover:scale-110 hover:bg-blue-500 transition duration-500 ease-in-out">{{ $permission->name }}</p>
+                    @endif
+                @endforeach
+            </div>
+        @endforeach
         </div>
     </div>
 </x-app-layout>
