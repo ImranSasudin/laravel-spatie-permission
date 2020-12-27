e791af3bfa7956b87325f00c4bdc32ffc3b3423f
ImranSasudin <imransasudin@gmail.com>
2020-12-27 14:22:13 +0800

    Update route

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function (){
+    Route::get('/dashboard', 'UserController@dashboard')->name('dashboard');
+});
 
 require __DIR__.'/auth.php';
